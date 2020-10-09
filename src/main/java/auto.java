7bd623c47e76a4e77ef9dc2e111ce29519d2c1b2
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2020-10-09 15:18:42 +0300

    tehtävä1 alku

---
+++ src/main/java/auto.java
@@ -0,0 +1,20 @@
+public class auto extends teht1 {
+
+    boolean ehjä;
+    boolean varattu;
+    public auto(boolean ehjä, boolean varattu) {
+        this.ehjä = ehjä;
+        this.varattu = varattu;
+    }
+
+    public boolean annaEhjä() {
+        return ehjä;
+    }
+    public boolean annaVarattu() {
+        return varattu;
+    }
+
+    public void asetaEhjä(boolean ehjä){
+        this.ehjä = ehjä;
+    }
+}
