f3fe1c3603a3b23b5d52851b019c162b63ab28a2
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2020-10-12 14:49:04 +0300

    tehtävä2 kylttiluokka + muuta

--- src/main/java/teht2.java
+++ src/main/java/teht2.java
@@ -1,8 +1,47 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class teht2 {
 
     public static void main(String[] args) {
+
+        Kyltti kyltti1;
+        kyltti1 = luokyltti(kyltti1);
+        tulosta(kyltti1);
+        kylttimuutos(kyltti1);
+
+        System.out.println("Haluatko luoda toisen kyltin? (vastaa 'kyllä' tai 'ei')");
+        Scanner scanner = new Scanner(System.in);
+        String vastaus1 = scanner.nextLine();
+        if (vastaus1.equals("ei")) {
+            System.exit(0);
+        }
+        else {
+            Kyltti kyltti2;
+            kyltti2 = luokyltti(kyltti2);
+            tulosta(kyltti2);
+            kylttimuutos(kyltti2);
+        }
+
+        System.out.println("Haluatko luoda kolmannen kyltin? (vastaa 'kyllä' tai 'ei')");
+        Scanner scanneri = new Scanner(System.in);
+        String vastaus2 = scanneri.nextLine();
+        if (vastaus2.equals("ei")) {
+            System.exit(0);
+        }
+        else {
+            Kyltti kyltti3;
+            kyltti3 = luokyltti(kyltti3);
+            tulosta(kyltti3);
+            kylttimuutos(kyltti3);
+        }
+
+        System.out.println("Haluatko vielä muokata jotain kylttiä? Kirjoita kyltin numero (1,2,3) tai 'ei'");
+        Scanner skanner = new Scanner(System.in);
+        String vastaus3 = skanner.nextLine();
+
+    }
+
+    public static void luokyltti (Kyltti kyltti) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Anna leveys välillä 8-40");
@@ -14,36 +53,64 @@ public class teht2 {
         String teksti = scanner.nextLine();
         System.out.println("Valitse täyte (*, # tai -)");
         String täyte = scanner.nextLine();
-
-        tulosta(leveys, korkeus, teksti, täyte);
-
-        System.out.println("Haluatko muuttaa täytettä? (*, # tai -) Kirjoita 'ei' jos haluat lopettaa.");
-
-        String uusitäyte = scanner.nextLine();
-        if (uusitäyte.equals("ei")){
-            System.exit(0);
-        } else {
-            tulosta(leveys, korkeus, teksti, uusitäyte);
-        }
     }
 
 
-    public static void tulosta (final int leveys, final int korkeus, final String teksti, final String täyte){
-        final int k2 = (korkeus - 1) / 2;
-        final int l2 = (leveys - teksti.length() - 2) / 2;
+    public static void kylttimuutos (Kyltti kyltti) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Haluatko muuttaa täytettä? (*, # tai -) Kirjoita 'ei' jos haluat lopettaa.");
+
+        String uusitäyte = scanner.nextLine();
+        if (uusitäyte.equals("ei")) {
+            System.exit(0);
+        } else {
+            tulosta(kyltti.annaLeveys(), kyltti.annaKorkeus(), kyltti.annaTeksti(), uusitäyte);
+        }
+
+        System.out.println("Haluatko muuttaa leveyttä? Kirjoita uusi leveys välillä 8-40, tai '0' jos haluat lopettaa");
+
+        int uusileveys = scanner.nextInt();
+        if (uusileveys == 0) {
+            System.exit(0);
+        } else {
+            tulosta(uusileveys, kyltti.annaKorkeus(), kyltti.annaTeksti(), uusitäyte);
+        }
+
+        System.out.println("Haluatko muuttaa korkeutta? Kirjoita uusi korkeus välillä 3-6, tai '0' jos haluat lopettaa");
+
+        int uusikorkeus = scanner.nextInt();
+        if (uusikorkeus == 0) {
+            System.exit(0);
+        } else {
+            tulosta(uusileveys, uusikorkeus, kyltti.annaTeksti(), uusitäyte);
+        }
+
+        System.out.println("Haluatko muuttaa vielä kerran täytettä? Kirjoita uusi täyte (*, # tai -) tai 'ei' jos lopetat");
+
+        String uusiuusitäyte = scanner.nextLine();
+        if (uusiuusitäyte.equals("ei")) {
+            System.exit(0);
+        } else {
+            tulosta(uusileveys, uusikorkeus, kyltti.annaTeksti(), uusiuusitäyte);
+        }
+    }
+
+    public static void tulosta (Kyltti kyltti){
+        final int k2 = (kyltti.annaKorkeus() - 1) / 2;
+        final int l2 = (kyltti.annaLeveys() - kyltti.annaTeksti().length() - 2) / 2;
         String r = "";
         //aluksin luodaan "ylärivi"
-        for (int i = 0; i < leveys; i++) r += täyte;
+        for (int i = 0; i < kyltti.annaLeveys(); i++) r += kyltti.annaTäyte();
         //tulostetaan ylärivi(t)
         for (int i = 0; i < k2; i++) System.out.println(r);
         //tulostetaan tekstin vasemmassa reunassa oleva täyte
-        for (int i = 0; i < l2; i++) System.out.print(täyte);
+        for (int i = 0; i < l2; i++) System.out.print(kyltti.annaTäyte());
         //tulostetaan haluttu teksti
-        System.out.print(" " + teksti + " ");
+        System.out.print(" " + kyltti.annaTeksti() + " ");
         //tulostetaan tekstin oikean puoleinen täyte
-        for (int i = 0; i < leveys - l2 - teksti.length() - 2; i++) System.out.print(täyte);
+        for (int i = 0; i < kyltti.annaLeveys() - l2 - kyltti.annaTeksti().length() - 2; i++) System.out.print(kyltti.annaTäyte());
         System.out.println();
         //tulostetaan alarivi(t)
-        for (int i = 0; i < korkeus - k2 - 1; i++) System.out.println(r);
+        for (int i = 0; i < kyltti.annaKorkeus() - k2 - 1; i++) System.out.println(r);
     }
 }
